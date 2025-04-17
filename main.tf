a848709e1b917bceea703d7981c17863a94db239
ErebDuin <163612858+ErebDuin@users.noreply.github.com>
2025-04-16 18:02:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "web_server" {
               mkswap /var/lib/swap/swapfile
               swapon /var/lib/swap/swapfile
               PUBLIC_IP=$(curl -s https://api.ipify.org)
-              curl -X GET "https://devops:11653bd900a61a61152c8caab3b8f24def@jenkins-ops.portnov.com/job/ansible-task/buildWithParameters?token=Abc123456&MANAGED_HOST=$PUBLIC_IP"
+              curl -X GET "https://devops:11653bd900a61a61152c8caab3b8f24def@jenkins-ops.portnov.com/job/gegam_ansible_task/buildWithParameters?token=Abc123456&MANAGED_HOST=$PUBLIC_IP"
               EOF
 }
 
